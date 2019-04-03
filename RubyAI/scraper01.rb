ab9bdcb6b2d9aa78fbce5ff4518b3d761426ae8e
sansoo <2018ndss@gmail.com>
2019-04-04 08:02:20 +0900

    20190404

---
+++ RubyAI/scraper01.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
+def scraper
+    url = "http://friendlibrary.ga"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    byebug
+end
+
+scraper
+=begin
+https://www.youtube.com/watch?v=b3CLEUBdWwQ
+ruby scrapper.rb
+unparsed_page
+parsed_page
+=end
